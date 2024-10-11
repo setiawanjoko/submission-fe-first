6ef6d4c500f938f86e37e5197679df5f12b9ea7c
setiawanjoko <setyawanj65@gmail.com>
2024-10-11 21:14:40 +0700

    Add | Add note

--- src/assets/js/validations/index.js
+++ src/assets/js/validations/index.js
@@ -1,3 +1,4 @@
+import { STORE_EVENT } from "../common.js"
 import { customValidationBodyHandler } from "./bodyValidation.js"
 import { blurValidationHandler } from "./realtimeValidate.js"
 import { customValidationTitleHandler } from "./titleValidation.js"
@@ -10,7 +11,8 @@ form.addEventListener('submit', e => {
     e.preventDefault()
     const title = titleInput.value
     const body = bodyInput.value
-    console.log({title, body})
+    
+    document.dispatchEvent(new CustomEvent(STORE_EVENT, {detail: {title: title, body: body}}))
 })
 titleInput.addEventListener('change', customValidationTitleHandler)
 titleInput.addEventListener('invalid', customValidationTitleHandler)
