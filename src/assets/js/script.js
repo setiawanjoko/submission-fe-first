788aa9d03d89b921c86999f2b78e9e1dd1af9eb2
setiawanjoko <setyawanj65@gmail.com>
2024-10-11 22:52:01 +0700

    Add | Webpack support

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -1,10 +1,11 @@
+import '../css/style.css'
 import { LS_SCOPE, LS_SORT } from "./common.js"
 import './components/index.js'
 import './validations/index.js'
 
 if(typeof Storage !== 'undefined') {
     if(localStorage.getItem(LS_SCOPE) === null) {
-        localStorage.setItem(LS_SCOPE, 0)
+        localStorage.setItem(LS_SCOPE, "false")
     }
     if(localStorage.getItem(LS_SORT) === null) {
         localStorage.setItem(LS_SORT, 'asc')
