1395eea6215ea4a71266f016c2d28ea61de1f9eb
setiawanjoko <setyawanj65@gmail.com>
2024-10-11 21:04:38 +0700

    Add | Custom exceptions

---
+++ src/assets/js/exceptions.js
@@ -0,0 +1,28 @@
+import Swal from 'sweetalert2'
+
+class ClientError extends Error {
+    constructor(message) {
+        super(message)
+
+        this.name = "ClientError"
+    }
+}
+
+class ServerError extends Error {
+    constructor(message) {
+        super(message)
+
+        this.name = "ServerError"
+    }
+}
+
+class ValidationError extends Error {
+    constructor(message) {
+        super(message)
+
+        this.name = "ValidationError"
+    }
+}
+
+
+export {ClientError, ServerError, ValidationError}
